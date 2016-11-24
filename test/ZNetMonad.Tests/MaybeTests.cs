5a35311eb2dd613d3be6e4793ff468531b3ae960
ZidaneFF <ultimecia_20@hotmail.com>
2016-11-24 19:46:11 +0100

    Add tests to better showcase usage of Maybe

--- test/ZNetMonad.Tests/MaybeTests.cs
+++ test/ZNetMonad.Tests/MaybeTests.cs
@@ -18,7 +18,7 @@ namespace ZNetMonad.Tests
 
         [Theory]
         [InlineData(1, 1)]
-        public void SumInIdentityBehavesAsExpected(int a, int b)
+        public void SumInMaybeBehavesAsExpected(int a, int b)
         {
             var m = from x in a.ToMaybe()
                     from y in b.ToMaybe()
@@ -27,5 +27,37 @@ namespace ZNetMonad.Tests
             var result = Assert.IsType<Just<int>>(m);
             Assert.Equal(a + b, result.Value);
         }
+
+        [Theory]
+        [InlineData(1)]
+        public void DivisionByZeroReturnsNothing(int a)
+        {
+            var m = from x in a.ToMaybe()
+                    from y in Div(a, 0)
+                    select y;
+
+            Assert.IsType<Nothing<int>>(m);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(10)]
+        public void DivisionByTwoBehavesAsExpected(int a)
+        {
+            var m = from x in a.ToMaybe()
+                    from y in Div(a, 2)
+                    select y;
+
+            var result = Assert.IsType<Just<int>>(m);
+            Assert.Equal(a / 2, result.Value);
+        }
+
+        private static Maybe<int> Div(int a, int b)
+            => b == 0
+                  ? new Nothing<int>()
+                  : (a / b).ToMaybe();
     }
 }
